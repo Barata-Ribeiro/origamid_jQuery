e8ae3fa4b307414da43aa77d76d2b1eabf3a00f5
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2025-02-11 08:34:53 -0300

    4-2 Adicionar Classe ao Clique - Otimizado

--- js/app.js
+++ js/app.js
@@ -1,27 +1,25 @@
 $(document).ready(function() {
-    const activeClass = "active";
-
-    $(".animais .tab-menu a").first().addClass(activeClass);
-    $(".animais .item").first().addClass(activeClass);
-
-    $(".animais .tab-menu a").click(function(event) {
-        event.preventDefault();
-        const itemId = $(this).attr("href");
-
-        $(".animais .tab-menu a, .animais .item").removeClass(activeClass);
-        $(this).addClass(activeClass);
-        $(itemId).addClass(activeClass);
+    
+    $("[data-group]").each(function() {
+        const $allTargets = $(this).find("[data-target]"),
+              $allClicks = $(this).find("[data-click]"),
+              activeClass = "active";
+        
+        $allTargets.first().addClass(activeClass);
+        $allClicks.first().addClass(activeClass);
+        
+        $allClicks.click(function(event) {
+            event.preventDefault();
+            
+            const id = $(this).data("click"),
+                  $target = $(`[data-target=${id}`);
+            
+            $allClicks.removeClass(activeClass);
+            $allTargets.removeClass(activeClass);
+            
+            $target.addClass(activeClass);
+            $(this).addClass(activeClass);
+        });
     });
-
-    $(".florestas .tab-menu a").first().addClass(activeClass);
-    $(".florestas .item").first().addClass(activeClass);
-
-    $(".florestas .tab-menu a").click(function(event) {
-        event.preventDefault();
-        const itemId = $(this).attr("href");
-
-        $(".florestas .tab-menu a, .florestas .item").removeClass(activeClass);
-        $(this).addClass(activeClass);
-        $(itemId).addClass(activeClass);
-    });
+    
 });
